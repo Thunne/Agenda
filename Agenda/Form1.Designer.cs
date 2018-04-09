b4d2b00067833b1920bb768b3222e8baec08568c
JanakanNan <j.nandakumaran@insta.fr>
2018-04-09 10:16:03 +0200

    Calendar graphique

--- Agenda/Form1.Designer.cs
+++ Agenda/Form1.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            //Label1 Date
+            this.label1 = new LabelDate(31);
+            for (int i = 0; i < 30; i++)
+            {
+                this.label1 = new LabelDate(i);
+            }
+
+            //Label2 Date
+            this.label2 = new LabelDate(31);
+            for (int i = 0; i < 30; i++)
+            {
+                this.label2 = new LabelDate(i);
+            }
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+
+           
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 426);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(773, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Agenda";
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private LabelDate[] Labels;
+        private System.Windows.Forms.Label label2;
     }
 }
 
---
+++ Agenda/LabelDate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    class LabelDate : Label
+    {
+        private int val;
+        
+        public int Value
+        {
+            get
+            {
+                return val;
+            }
+            set
+            {
+                val = value;
+            }
+        }
+
+        public LabelDate(int n)
+        {
+            val = n;
+            this.Text = n.ToString();
+            this.Size = new System.Drawing.Size(50, 50);
+        }
+    }
+}
